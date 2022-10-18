e012e67f7b9933c1222ef93456e25448c6c8041e
GoksuCetintas <goksucetintas@gmail.com>
2022-10-18 15:11:16 +0300

    Classes are added

---
+++ CafeBoston.DATA/Order.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeBoston.DATA
+{
+    public class Order
+    {
+        public int TableNo { get; set; }
+        public OrderState State { get; set; } = OrderState.Active;
+        public DateTime? StartTime { get; set; } = DateTime.Now;
+        public DateTime? EndTime { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+
+
+        public string TotalPriceYTL { get { return "asdf"; } }
+        public decimal TotalPrice()
+        {
+            decimal TotalPrice=0;
+            foreach (var item in OrderDetails)
+            {
+                TotalPrice += item.TotalPrice();
+            }
+            return TotalPrice;
+        }
+        public decimal PaidAmount { get;set; }
+    }
+}
+
+
+
+
